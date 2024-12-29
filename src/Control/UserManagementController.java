826de756b14d05fda02ae4edec70cc65dfba3809
TheButcher13 <muka.arjan@outlook.com>
2024-12-29 12:26:11 +0100

    Arjan's Model Classes

---
+++ src/Control/UserManagementController.java
@@ -0,0 +1,102 @@
+package Control;
+
+import java.util.*;
+
+public class UserManagementController {
+    private String role;
+    private ArrayList<Permission> permissions;
+//    private Boolean hasARole = false;
+//    private Boolean costumPermissions = false; // make sure that no matter the role it might have custom permissions
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public ArrayList<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(ArrayList<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+//    public Boolean getCostumPermissions() {
+//        return costumPermissions;
+//    }
+//
+//    public void setCostumPermissions(Boolean customPermissions) {
+//        this.costumPermissions = customPermissions;
+//    }
+//
+//    public Boolean getHasARole() {
+//        return hasARole;
+//    }
+//
+//    public void setHasARole(Boolean hasARole) {
+//        this.hasARole = hasARole;
+//    }
+//
+//    public Boolean assignUserRole() {
+//        if (getRole() == null || getRole().equals("Invalid")) { // Doesn't have a role/ it is not a (regular) user
+//            return false;
+//        } else
+//            return true;
+//    }
+
+    // Granting permissions
+
+//    PermissionGrantingController p = new PermissionGrantingController();
+//
+//    public void initializePermissions() {
+//        hasARole = assignUserRole(); // Chooses what permissions to give initially
+//        if (hasARole == true) {
+//            if(getCostumPermissions() == true){
+//                permissions = p.giveCustomPermissions();
+//            }
+//            else if (getRole().equals("Cashier")) {
+//                permissions = p.giveDefaultCashierPermissions();
+//            }
+//            else if(getRole().equals("Manager")){
+//                permissions = p.giveDefaultManagerPermissions();
+//            }
+//        }
+//    }
+
+    // Administrator adds permissions
+    public void grantPermission(Permission permission) {
+        permissions.add(permission);
+    }
+
+    // Administrator removes permissions
+    public void revokePermission(Permission permission) {
+        permissions.remove(permission);
+    }
+
+    public Boolean validatePasswordStrength(String password) {
+        if (password.length() < 10) {
+            return false;
+        }
+        Boolean hasUppercase = false;
+        Boolean hasLowercase = false;
+        Boolean hasDigit = false;
+        Boolean hasSpecialChar = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                hasUppercase = true;
+            } else if (Character.isLowerCase(ch)) {
+                hasLowercase = true;
+            } else if (Character.isDigit(ch)) {
+                hasDigit = true;
+            } else if (!Character.isLetterOrDigit(ch)) {
+                hasSpecialChar = true;
+            }
+        }
+        return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+    }
+
+}
---
+++ src/Model/Supplier.java
@@ -0,0 +1,102 @@
+package Model;
+
+public class Supplier {
+    private int supplierId;
+    private String companyName;
+    private String contactName;
+    private String email;
+    private long phoneNumber;
+    private String address;
+    private String paymentDetailsIBAN;
+    private ArrayList<Item> productList;
+    private int minimumOrderQuantity;
+
+    public Supplier() {
+    }
+
+    public Supplier(int supplierId, String companyName, String contactName, String email, long phoneNumber, String address,
+                    String paymentDetailsIBAN, ArrayList<Item> productList, int minimumOrderQuantity) {
+        this.supplierId = supplierId;
+        this.companyName = companyName;
+        this.contactName = contactName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.paymentDetailsIBAN = paymentDetailsIBAN;
+        this.productList = productList;
+        this.minimumOrderQuantity = minimumOrderQuantity;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPaymentDetailsIBAN() {
+        return paymentDetailsIBAN;
+    }
+
+    public void setPaymentDetailsIBAN(String paymentDetailsIBAN) {
+        this.paymentDetailsIBAN = paymentDetailsIBAN;
+    }
+
+    public ArrayList<Item> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ArrayList<Item> productList) {
+        this.productList = productList;
+    }
+
+    public int getMinimumOrderQuantity() {
+        return minimumOrderQuantity;
+    }
+
+    public void setMinimumOrderQuantity(int minimumOrderQuantity) {
+        this.minimumOrderQuantity = minimumOrderQuantity;
+    }
+}
+
