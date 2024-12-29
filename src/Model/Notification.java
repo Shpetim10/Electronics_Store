7446a4c3355f73400d35c87ef96b05e18af7d1d4
Shpetim10 <shabanajshpetim@gmail.com>
2024-12-29 18:36:07 +0100

    Updated user class, notification constructors and added OTHER as NotificationType

--- src/Model/Notification.java
+++ src/Model/Notification.java
@@ -3,26 +3,21 @@ package Model;
 import java.util.Date;
 
 public class Notification  {
-    private int id;
     private String message;
     private Date dateCreated;
     private boolean isRead;
-    private Notification subject;
-
-    public Notification() {};  //No arguments constructor
-public Notification(int id,String message,Date dateCreated,  //All arguments constructor
-                    boolean isRead,Notification subject){
-    this.id=id;
-    this.message=message;
-    this.dateCreated=dateCreated;
-    this.isRead=isRead;
-    this.subject=subject;
-}
+    private NotificationType subject;
 
 
-    public int getId() {return this.id;}
-    public void setId(int id) {
-        this.id = id;
+    public Notification(NotificationType subject, String message){
+        this.subject=subject;
+        this.message=message;
+        this.isRead=false;
+        this.dateCreated=new Date();
+    }
+
+    public Notification(String message){
+        this(NotificationType.OTHER,message);
     }
 
     public String getMessage() {return this.message;}
@@ -42,8 +37,8 @@ public Notification(int id,String message,Date dateCreated,  //All arguments con
         this.isRead = isRead;
     }
 
-    public Notification getSubject() {return this.subject;}
-    public void setSubject(Notification subject) {
+    public NotificationType getSubject() {return this.subject;}
+    public void setSubject(NotificationType subject) {
         this.subject = subject;
     }
 
